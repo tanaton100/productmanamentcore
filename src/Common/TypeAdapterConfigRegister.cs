b3f11006e11010ed9d1d08c7c6655608508747b3
tanaton <tanaton.akko@gmail.com>
2021-03-07 14:58:57 +0700

    Add mapster simple

---
+++ src/Common/TypeAdapterConfigRegister.cs
@@ -0,0 +1,18 @@
+﻿using Mapster;
+using ProductmanagementCore.Models;
+using ProductmanagementCore.Models.Dto;
+namespace ProductmanagementCore.Common
+{
+    public  class TypeAdapterConfigRegister : IRegister
+    {
+
+
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Users, UserDto>()
+                .Map(d => d.Id, s => s.Id)
+                .Map(d => d.FullName, s => $"{s.Firstname} {s.Lastname}")
+                .Map(d => d.Products, s => s.Products);//lookup 
+        }
+    }
+}
---
+++ src/Models/Dto/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ProductmanagementCore.Models.Dto
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+        public IEnumerable<Products> Products { get; set; }
+
+    }
+}
