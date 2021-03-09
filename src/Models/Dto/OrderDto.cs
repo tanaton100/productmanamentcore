103f8fd88f1a617d41e1e49e17c99fbbfe8d939e
tanaton <tanaton.akko@gmail.com>
2021-03-09 17:50:05 +0700

    add orderView

---
+++ src/Models/Dto/OrderDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductmanagementCore.Models.Dto
+{
+    public class OrderDto
+    {
+        public int OrderId { get; set; }
+        public string UserName { get; set; }
+        public string ProductName { get; set; }
+    }
+}
